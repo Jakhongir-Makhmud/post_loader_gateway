521df1ef847c37ff64d37c6821d66977e83a8339
Jakhongir <anorboev.jahongir8007@gmail.com>
2022-07-12 15:31:03 +0500

    modified router.go

--- api/router.go
+++ api/router.go
@@ -21,13 +21,13 @@ func NewRouter(params Params) *mux.Router {
 	r := mux.NewRouter()
 	handlers := handlers.NewHandlers(handlers.Params{Logger: params.Logger, Cfg: params.Cfg, Services: params.Services})
 
-	r.HandleFunc("/post/get", middleware.ApplyMiddleware(handlers.PostHandler.GetPost, handlers.Middleware.LogRequest))
-	r.HandleFunc("/post/update", middleware.ApplyMiddleware(handlers.PostHandler.UpdatePost, handlers.Middleware.LogRequest))
-	r.HandleFunc("/post/delete/{id}", middleware.ApplyMiddleware(handlers.PostHandler.DeletePost, handlers.Middleware.LogRequest))
-	r.HandleFunc("/post/list", middleware.ApplyMiddleware(handlers.PostHandler.GetPosts, handlers.Middleware.LogRequest))
+	r.HandleFunc("/post/get", middleware.ApplyMiddleware(handlers.PostHandler.GetPost, handlers.Middleware.LogRequest)).Methods("GET")
+	r.HandleFunc("/post/update", middleware.ApplyMiddleware(handlers.PostHandler.UpdatePost, handlers.Middleware.LogRequest)).Methods("UPDATE")
+	r.HandleFunc("/post/delete/{id}", middleware.ApplyMiddleware(handlers.PostHandler.DeletePost, handlers.Middleware.LogRequest)).Methods("DELETE")
+	r.HandleFunc("/post/list", middleware.ApplyMiddleware(handlers.PostHandler.GetPosts, handlers.Middleware.LogRequest)).Methods("POST")
 
-	r.HandleFunc("/load/posts", middleware.ApplyMiddleware(handlers.PostLoaderHandler.LoadPosts, handlers.Middleware.LogRequest))
-	r.HandleFunc("/load/status/{id}", middleware.ApplyMiddleware(handlers.PostLoaderHandler.GetLoadingStatus, handlers.Middleware.LogRequest))
+	r.HandleFunc("/load/posts", middleware.ApplyMiddleware(handlers.PostLoaderHandler.LoadPosts, handlers.Middleware.LogRequest)).Methods("POST")
+	r.HandleFunc("/load/status/{id}", middleware.ApplyMiddleware(handlers.PostLoaderHandler.GetLoadingStatus, handlers.Middleware.LogRequest)).Methods("GET")
 
 	return r
 }
